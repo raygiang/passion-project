a2231330a3ae892b4513f5c6584c487af6a2c99c
Ray <raymondgiang@gmail.com>
2019-02-07 06:47:05 -0500

    Add project files.

---
+++ StoryWriting_n01304390/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StoryWriting_n01304390
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ StoryWriting_n01304390/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoryWriting_n01304390.MvcApplication" Language="C#" %>
